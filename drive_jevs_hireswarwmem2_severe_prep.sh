7310f37de518284a93cc30203123ad9d310a59a9
LoganDawson-NOAA <Logan.Dawson@noaa.gov>
2023-04-21 15:52:25 +0000

    update crons

--- drive_jevs_hireswarwmem2_severe_prep.sh
+++ drive_jevs_hireswarwmem2_severe_prep.sh
@@ -13,14 +13,10 @@ cd /lfs/h2/emc/ptmp/${USER}/cron.out
 
 module reset
 
-if [ $hh -lt 06 ]; then
+if [ $hh -lt 12 ]; then
    vhr=00
-elif [ $hh -ge 06 ] && [ $hh -lt 12 ]; then
-   vhr=06
-elif [ $hh -ge 12 ] && [ $hh -lt 18 ]; then
+elif [ $hh -gt 12 ]; then
    vhr=12
-elif [ $hh -gt 18 ]; then
-   vhr=18
 fi
 
 qsub -v cyc=$vhr /lfs/h2/emc/vpppg/save/logan.dawson/EVS/ecf/cam/prep/jevs_hireswarwmem2_severe_prep.ecf
